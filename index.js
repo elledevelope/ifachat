402f1080f45a51b26e269cf7f4dcaf6faa9ccc5c
elmira <elledevelope@gmail.com>
2024-10-30 12:53:11 +0100

    Created users-currently-online-list using html ul

--- index.js
+++ index.js
@@ -2,55 +2,59 @@ const express = require("express");
 const http = require('http');
 const app = express();
 const server = http.Server(app);
-const io = require('socket.io')(server)
+const io = require('socket.io')(server);
 //172.20.192.227
 
 const ip = "127.0.0.1";
 const port = 4000;
 app.use(express.static('public'));
-app.get('/',(req,res)=>{
-   res.sendFile('index.html',{root:__dirname}); 
-})
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
 // un tableau pour suivre mes utilisateurs actifs
 const users = [];
 const publicMessages = [];
 
-//.....
-io.on("connection",(socket)=>{
+//............................................................. SERVER-SIDE :
+io.on("connection", (socket) => {
 
-    socket.emit("init",{message:"bienvenue cher client du chat"})
+    socket.emit("init", { message: "Bienvenue cher client du chat" })
 
-    // attente de l'emit sendLog
-    socket.on("sendLog",(data)=>{
+    // ------------------ attente de l'emit sendLog from the client = script.js
+    socket.on("sendLog", (data) => {
         //console.log(socket.id);
-        // securisation par authenticator ...
+        // TO ADD HERE LATER: -------- sécurisation par authenticator ...
         data.id = socket.id;
         users.push(data);
         //console.dir(users);
-    })
-    socket.on("publicMessage",(data)=>{
+
+        io.emit("updateUserList", users); // sending users-currently-connected-list from the client = script.js
+    });
+
+    // ------------------ publicMessage
+    socket.on("publicMessage", (data) => {
         data.id = socket.id;
         publicMessages.push(data);
         //console.dir(publicMessages);
-        socket.broadcast.emit("publicMessageGlobal",data);
-    })
-    socket.on("disconnect",()=>{
+        socket.broadcast.emit("publicMessageGlobal", data);
+    });
+
+    // ------------------ remove the disconnected user
+    socket.on("disconnect", () => {
         //socket.id
         let indexDisconnect;
-        users.forEach((element,index) => {
-            if(element.id === socket.id){
+        users.forEach((element, index) => {
+            if (element.id === socket.id) {
                 indexDisconnect = index;
-            }
+            };
         });
-        // splice sert à supprimer une entrée de tableau à
-        // partir de son index (indexDisconnect)
-        users.splice(indexDisconnect,1);
+        // splice sert à supprimer une entrée de tableau à partir de son index (indexDisconnect)
+        users.splice(indexDisconnect, 1);
         console.dir(users);
-        
-    })
-})
+    });
+});
 
-server.listen(port,ip,()=>{
-    console.log("Demarer sur http://"+ip+":"+port);
-    
-})
+
+server.listen(port, ip, () => {
+    console.log("Démarrer sur http://" + ip + ":" + port);
+});
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,6 @@
 // declaration de variables
+const userList = document.getElementById("userList");
+
 const sendPublic = document.querySelector("#sendPublic");
 const messagesPublic = document.getElementById("messagesPublic");
 const socket = io();
@@ -8,15 +10,17 @@ const pseudo = urlParams.get("pseudo");
 console.log(pseudo);
 const pwd = urlParams.get("pwd");
 console.log(pwd);
+
 // declarations de fonction
-const displayMessage = (data)=>{
+const displayMessage = (data) => {
     messagesPublic.innerHTML += `
     <div class="newMessage">
         <h2>${data.pseudo}</h2>
         <p class="content">${data.messageContent}</p>
         <p class="date">${data.date}</p>
     </div>`
-}
+};
+
 tinymce.init({
     selector: '#textPublic',
     plugins: [
@@ -29,23 +33,41 @@ tinymce.init({
         'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
 });
 
+///////////////--------------------------- Users-currently-online-list : 
+const renderUserList = (users) => {
+    userList.innerHTML = ""; // start with empty list of users
+    users.forEach((user) => {
+        const userElement = document.createElement("li");
+        userElement.textContent = user.pseudo;
+        userList.appendChild(userElement);
+    });
+    // console.log(users.pseudo);
+};
+
+///////////////--------------------------- Receive the updated users list :
+socket.on("updateUserList", (users) => {
+    renderUserList(users);
+});
+
 socket.on("init", (data) => {
     console.dir(data);
     socket.emit("sendLog", {
         pseudo: pseudo,
         pwd: pwd
     });
-})
-sendPublic.addEventListener("click",()=>{
+});
+
+sendPublic.addEventListener("click", () => {
     let messageContent = tinyMCE.get("textPublic").getContent();
     let date = new Date();//UTC ou https://momentjs.com/
     // envoie du message public au server
-    let data = {pseudo:pseudo,messageContent:messageContent,date:date}
-    socket.emit("publicMessage",data);
+    let data = { pseudo: pseudo, messageContent: messageContent, date: date }
+    socket.emit("publicMessage", data);
     displayMessage(data);
+});
 
-})
-socket.on("publicMessageGlobal",(data)=>{
+
+socket.on("publicMessageGlobal", (data) => {
     console.dir(data);
     displayMessage(data);
-})
+});
